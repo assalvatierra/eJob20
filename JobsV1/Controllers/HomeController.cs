8d7085081ec121c15014e4fe51725125b88d1322
Abel S. Salvatierra <assalvatierra@gmail.com>
2019-03-04 19:53:36 +0800

    etc

--- JobsV1/Controllers/HomeController.cs
+++ JobsV1/Controllers/HomeController.cs
@@ -8,9 +8,48 @@ namespace JobsV1.Controllers
 {
     public class HomeController : Controller
     {
+        JobsV1.SysAccessLayer sysdb = new SysAccessLayer();
+
         [Authorize]
+        [HandleError(View = "Error")]
         public ActionResult Index()
         {
+
+            ViewBag.Message = "Main page";
+            ViewBag.EntHomePageLogo = "Images/EntityFiles/GAC/gaclogo.jpg"; //to update
+            ViewBag.EntHomeBackgroundImage = "Images/EntityFiles/GAC/home_background.jpg"; //to update
+
+            ViewBag.TextClass = "textClass_dark";
+            ViewBag.EntName = "Real Breeze Travel and Tours";
+            ViewBag.EntAddress1 = "2/F Prime-Z Bldg, McArthur Hway, Davao city, Philipines ";
+            ViewBag.EntAddress2 = "M: 0916-755-8473, 0926-733-5449, 0909-076-1575";
+            ViewBag.EntAddress3 = "E: RealBreezeDavao@gmail.com, Travel.RealBreeze@gmail.com";
+            ViewBag.EntAddress4 = "";
+            ViewBag.EntAddress5 = "";
+
+            ViewBag.EntServices = sysdb.getServiceModules(User.Identity.Name);
+
+            //get current application version
+            string sConfigVersion = "";
+            string sDBActive = "+++";
+            try
+            {
+                sConfigVersion = System.Configuration.ConfigurationManager.AppSettings["ConfigVersion"].ToString();
+            }
+            catch { }
+            ViewBag.ConfigVersion = sConfigVersion;
+
+            //try
+            //{
+            //    sDBActive = sysdb.getSysSetting("DatabaseState");
+            //}
+            //catch { }
+            ViewBag.DBActive = sDBActive;
+
+
+
+
+
             return View();
         }
 
--- JobsV1/Models/SysDB.Designer.cs
+++ JobsV1/Models/SysDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Data\Real\Apps\GitHub\eJob2\JobsV1\Models\SysDB.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Data\Real\Apps\GitHub\eJob20\JobsV1\Models\SysDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
