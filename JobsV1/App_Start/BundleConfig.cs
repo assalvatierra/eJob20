9a888320c76e981cb30af67567d41e5226cd9846
Abel S. Salvatierra <assalvatierra@gmail.com>
2019-03-05 15:10:57 +0800

    added themes

--- JobsV1/App_Start/BundleConfig.cs
+++ JobsV1/App_Start/BundleConfig.cs
@@ -32,6 +32,29 @@ namespace JobsV1
                       "~/Content/login.css",
                       "~/Content/site.css"));
 
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/core.css",
+                        "~/Content/themes/base/resizable.css",
+                        "~/Content/themes/base/selectable.css",
+                        "~/Content/themes/base/accordion.css",
+                        "~/Content/themes/base/autocomplete.css",
+                        "~/Content/themes/base/button.css",
+                        "~/Content/themes/base/dialog.css",
+                        "~/Content/themes/base/slider.css",
+                        "~/Content/themes/base/tabs.css",
+                        "~/Content/themes/base/datepicker.css",
+                        "~/Content/themes/base/progressbar.css",
+                        "~/Content/themes/base/theme.css"));
+
+            bundles.Add(new StyleBundle("~/Content/theme/darkness").Include(
+                "~/Content/themes/ui-darkness/jquery-ui.ui-darkness.css"
+               ));
+            bundles.Add(new StyleBundle("~/Content/theme/lightness").Include(
+                "~/Content/themes/ui-lightness/jquery-ui.ui-lightness.css"
+               ));
+
+
+
             bundles.Add(new ScriptBundle("~/bundles/DateRangePicker").Include(
                     "~/Scripts/DateRangePicker/moment.js",
                     "~/Scripts/DateRangePicker/daterangepicker.js"
