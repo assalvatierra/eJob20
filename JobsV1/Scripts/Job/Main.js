46f70eba17cb93aaca1d289a5fa14b4aca9b8a65
Jahdiel Villosa <jahdielsvillosa@gmail.com>
2019-03-25 10:51:05 +0800

    Script fix

--- JobsV1/Scripts/Job/Main.js
+++ JobsV1/Scripts/Job/Main.js
@@ -47,4 +47,77 @@ function InitDatePicker()
 }
 
 
+/* Input validation */
 
+$(document).ready(function () {
+    $("#CompanyId2").val(2);
+});
+
+//Change job desription bsed on the cutomer name
+$("#customerList").change(function () {
+    var customer = $("#customerList option:selected").text();
+    //console.log("customer : " + customer);
+    $("#jobdesc").val(customer);
+    getEmail();
+    getNumber();
+    getCompany();
+});
+
+//get email using ajax post
+function getEmail() {
+    $.post("/JobOrder/getCustomerEmail",
+    {
+        id: $("#customerList option:selected").val()
+    },
+    function (data, status) {
+        $("#CustContactEmail").val(data);
+        //  alert("Data: " + data + "\nStatus: " + status);
+    });
+}
+
+//get number using ajax post
+function getNumber() {
+    $.post("/JobOrder/getCustomerNumber",
+    {
+        id: $("#customerList option:selected").val()
+    },
+    function (data, status) {
+        $("#CustContactNumber").val(data);
+    });
+}
+
+//get number using ajax post
+function getCompany() {
+
+    $.post("/JobOrder/getCustomerCompany",
+    {
+        id: $("#customerList option:selected").val()
+    },
+    function (data, status) {
+        //alert("Data: " + data + "\nStatus: " + status);
+        $("#CompanyId").val(data);
+    });
+}
+
+function searchCustomer() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("SearchBarCustomer");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("SearchListCustomer");
+    li = ul.getElementsByTagName("li");
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+
+function setValue(value) {
+    $('#customerList').val(value);
+    $('#CustomersModal').modal('toggle');
+}
