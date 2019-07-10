0cf678052dbf30d9bc7a445298ad4238d14e1b3a
Jahdiel Villosa <jahdielsvillosa@gmail.com>
2019-07-10 10:14:17 +0800

    Job Expense Edit Date
    Add DateTimepicker on Edit date of Job Expenses

--- JobsV1/Scripts/Job/JobExpense.js
+++ JobsV1/Scripts/Job/JobExpense.js
@@ -21,15 +21,15 @@ function InitDatePicker()
         singleDatePicker: true,
         showDropdowns: true,
         locale: {
-            format: 'MM/DD/YYYY  hh:mm A'
+            format: 'MM/DD/YYYY hh:mm A'
         },
        
     },
     function (start, end, label) {
         //check if date is greater than or equal to today
         
-        var today = moment().format('YYYY-MM-DD');
-        var datepicker = start.format('YYYY-MM-DD');
+        var today = moment().format('YYYY-MM-DD hh:mm A');
+        var datepicker = start.format('YYYY-MM-DD hh:mm A');
         //alert(today > datepicker);
 
             //if (today > datepicker) {
@@ -45,4 +45,39 @@ function InitDatePicker()
     $('#DtExpense').val(ddd1);
     //$('input[name="JobDate"]').val(ddd1.substr(0, ddd1.indexOf(" ") ));
 
+
+     var ddd2 = $('#edit-DtExpense').val();
+
+     $('#edit-DtExpense').daterangepicker(
+    {
+        timePicker: false,
+        timePickerIncrement: 1,
+        singleDatePicker: true,
+        showDropdowns: true,
+        locale: {
+            format: 'MM/DD/YYYY hh:mm A'
+        },
+       
+    },
+    function (start, end, label) {
+        //check if date is greater than or equal to today
+        
+        var today = moment().format('YYYY-MM-DD hh:mm A');
+        var datepicker = start.format('YYYY-MM-DD hh:mm A');
+        //alert(today > datepicker);
+
+            //if (today > datepicker) {
+            //    alert("Job date is past the date today. Do you want to continue?");
+
+            //}
+
+        //alert(start.format('YYYY-MM-DD'));
+        
+        }
+     );
+
+     $('#edit-DtExpense').val(ddd2);
+    //$('input[name="JobDate"]').val(ddd1.substr(0, ddd1.indexOf(" ") ));
 }
+
+
