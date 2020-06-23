678e8d16086d0a013aef9b5d0f3034a52da46697
jahdielvillosa <jahdielsvillosa@gmail.com>
2020-06-23 21:38:54 +0800

    Copy Logs on CarRentalLogs Added

--- JobsV1/Areas/Personel/Controllers/CarRentalLogController.cs
+++ JobsV1/Areas/Personel/Controllers/CarRentalLogController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using JobsV1.Areas.Personel.Models;
+using Microsoft.Ajax.Utilities;
 
 namespace JobsV1.Areas.Personel.Controllers
 {
@@ -18,7 +20,7 @@ namespace JobsV1.Areas.Personel.Controllers
         public ActionResult Index()
         {
             var crLogTrips = db.crLogTrips.Include(c => c.crLogDriver).Include(c => c.crLogUnit).Include(c => c.crLogCompany).Include(c => c.crLogClosing);
-            return View(crLogTrips.ToList());
+            return View(crLogTrips.OrderByDescending(c=>c.DtTrip).ToList());
         }
 
         // GET: Personel/CarRentalLog/Details/5
@@ -250,6 +252,81 @@ namespace JobsV1.Areas.Personel.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+
+        public ActionResult CopyTrip()
+        {
+            ViewBag.isSubmitted = false;
+            return View(new List<crLogTrip>());
+        }
+
+        [HttpPost]
+        public ActionResult CopyTrip(string srchDate)
+        {
+            try
+            {
+                if (!srchDate.IsNullOrWhiteSpace())
+                {
+                    var tempDate = DateTime.Parse(srchDate);
+
+                    var crLogTrips = db.crLogTrips.Include(c => c.crLogDriver).Include(c => c.crLogUnit).Include(c => c.crLogCompany).Include(c => c.crLogClosing);
+
+                    crLogTrips = crLogTrips.Where(c => c.DtTrip.Day == tempDate.Day &&
+                                                        c.DtTrip.Month == tempDate.Month &&
+                                                        c.DtTrip.Year == tempDate.Year);
+                    ViewBag.isSubmitted = true;
+                    return View(crLogTrips.ToList());
+                }
+                ViewBag.isSubmitted = false;
+                return View(new List<crLogTrip>());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool CopyTripSubmit(int?  id, string copyDate)
+        {
+            try
+            {
+                if (copyDate.IsNullOrWhiteSpace() || id == null)
+                {
+                    return false;
+                }
+
+                var cDate = DateTime.ParseExact(copyDate , "MM/dd/yyyy", CultureInfo.InvariantCulture);
+
+                //find logs by id
+                var cLogTrip = db.crLogTrips.Find(id);
+
+                if (cLogTrip == null)
+                {
+                    return false;
+                }
+
+                crLogTrip newCrLogTrip = new crLogTrip();
+                newCrLogTrip.crLogCompanyId = cLogTrip.crLogCompanyId;
+                newCrLogTrip.crLogDriverId = cLogTrip.crLogDriverId;
+                newCrLogTrip.crLogUnitId = cLogTrip.crLogUnitId;
+                newCrLogTrip.DriverFee = cLogTrip.DriverFee;
+                newCrLogTrip.Expenses = cLogTrip.Expenses;
+                newCrLogTrip.Rate = cLogTrip.Rate;
+                newCrLogTrip.Remarks = cLogTrip.Remarks;
+                newCrLogTrip.Addon = cLogTrip.Addon;
+                newCrLogTrip.DtTrip = cDate.AddTicks(cLogTrip.DtTrip.TimeOfDay.Ticks);
+
+                db.crLogTrips.Add(newCrLogTrip);
+                db.SaveChanges();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
         }
     }
 }
---
+++ JobsV1/Areas/Personel/Script/CopyTrip.js
@@ -0,0 +1,50 @@
+﻿
+
+$(document).ready(function () {
+    InitDatePicker();
+})
+
+function InitDatePicker()
+{
+    var ddd1 = $('#findLogDate').val();
+
+    $('#findLogDate').daterangepicker(
+    {
+        timePicker: false,
+        timePickerIncrement: 1,
+        singleDatePicker: true,
+        showDropdowns: true,
+        locale: {
+            format: 'MM/DD/YYYY'
+        }
+    },
+    function (start, end, label) {
+        var today = moment().format('YYYY-MM-DD');
+        var datepicker = start.format('YYYY-MM-DD');
+         
+        }
+    );
+
+    $('#findLogDate').val(ddd1);
+
+    //Date 2
+    var ddd2 = $('#copiedLogDate').val();
+    $('#copiedLogDate').daterangepicker(
+        {
+            timePicker: false,
+            timePickerIncrement: 1,
+            singleDatePicker: true,
+            showDropdowns: true,
+            locale: {
+                format: 'MM/DD/YYYY'
+            }
+        },
+        function (start, end, label) {
+            var today = moment().format('YYYY-MM-DD');
+            var datepicker = start.format('YYYY-MM-DD');
+
+        }
+    );
+
+    $('#copiedLogDate').val(ddd2);
+}
