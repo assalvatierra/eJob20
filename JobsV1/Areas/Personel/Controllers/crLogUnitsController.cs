b32c2be9818b67a054370f56b29c111309518a0f
Abel Salvatierra <assalvatierra@gmail.com>
2020-07-23 19:33:39 +0800

    logUnit update

--- JobsV1/Areas/Personel/Controllers/crLogUnitsController.cs
+++ JobsV1/Areas/Personel/Controllers/crLogUnitsController.cs
@@ -47,7 +47,7 @@ namespace JobsV1.Areas.Personel.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Description")] crLogUnit crLogUnit)
+        public ActionResult Create([Bind(Include = "Id,Description,OrderNo")] crLogUnit crLogUnit)
         {
             if (ModelState.IsValid && InputValidation(crLogUnit))
             {
@@ -79,7 +79,7 @@ namespace JobsV1.Areas.Personel.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Description")] crLogUnit crLogUnit)
+        public ActionResult Edit([Bind(Include = "Id,Description,OrderNo")] crLogUnit crLogUnit)
         {
             if (ModelState.IsValid && InputValidation(crLogUnit))
             {
