32b0159e991df24c575847d77746e788743695cc
jahdielvillosa <jahdielsvillosa@gmail.com>
2020-07-27 19:43:24 +0800

    Car Rental Reports Initial

---
+++ JobsV1/Areas/Personel/Controllers/CarRentalReportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobsV1.Areas.Personel.Controllers
+{
+    public class CarRentalReportController : Controller
+    {
+        // GET: Personel/CarRentalReport
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
