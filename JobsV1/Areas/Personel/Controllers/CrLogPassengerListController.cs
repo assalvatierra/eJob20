b9bcf7ded61193f903d2335ee7736b271aa72a1e
Abel Salvatierra <assalvatierra@gmail.com>
2020-10-27 12:12:21 +0800

    pass list controller
    added passenger list controller - partial

---
+++ JobsV1/Areas/Personel/Controllers/CrLogPassengerListController.cs
@@ -0,0 +1,25 @@
+﻿using JobsV1.Areas.Personel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobsV1.Areas.Personel.Controllers
+{
+    public class CrLogPassengerListController : Controller
+    {
+        CarRentalLogDBContainer db = new CarRentalLogDBContainer();
+        // GET: Personel/CrLogPassengerList
+        public ActionResult Index()
+        {
+            List<crLogTrip> crTrip = db.crLogTrips
+                .Where(d => d.crLogPassengers.Count() > 0)
+                .Include(d=>d.crLogPassengers)
+                .ToList();
+
+            return View(crTrip);
+        }
+    }
+}
