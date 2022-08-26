64eb9d4accb615155c33979c4117770f3b16ecec
jahdiel <jahdielsvillosa@gmail.com>
2022-08-26 11:34:00 +0800

    Create Areas/Masterlists - > Cities

---
+++ JobsV1/Areas/Masterlists/MasterlistsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace JobsV1.Areas.Masterlists
+{
+    public class MasterlistsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Masterlists";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Masterlists_default",
+                "Masterlists/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
